3501ae04ce047d1643e789182ffae50545c44d09
Christian Drumm <drumm@fh-aachen.de>
2021-03-23 22:38:41 +0100

    fixed footer layout

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,14 +14,14 @@ const Footer = () => {
       <div className={`${myFooter} mx-auto`}>
         <Container>
           <Row >
-            <Col md={1}></Col>
+            <Col xs='auto'></Col>
             <Col className="text-center text-secondary">
               <IconContext.Provider value={{ color: "LightGreen" }}>
                 Made with lots of <a href='https://en.wikipedia.org/wiki/Green_tea' target='_blank' rel='noreferrer'>
                   <GiTeapotLeaves  aria-label='Tea Pot'/></a>.
               </IconContext.Provider>
             </Col>
-            <Col md={1} className="text-right text-secondary">
+            <Col xs='auto' className="text-right text-secondary">
               <IconContext.Provider value={{ className: `${rssIcon}` }}>
                 <Link to="/rss.xml">
                   <FaRss  aria-label='Link to RSS Feed'/>
