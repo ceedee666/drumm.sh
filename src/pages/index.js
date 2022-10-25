c1916d704882f5dd5ecd2b32fc67360118c51a40
Christian Drumm <christian.drumm@hey.com>
2022-10-25 16:58:11 +0200

    added github link

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Row, Col, Image } from "react-bootstrap";
 
 import { graphql, useStaticQuery } from "gatsby";
 
-import { SiTwitter, SiSap, SiYoutube } from "react-icons/si";
+import { SiTwitter, SiSap, SiYoutube, SiGithub } from "react-icons/si";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
@@ -24,80 +24,16 @@ const HomePage = () => {
       <SEO title="drumm.sh" />
       <Row>
         <Col md={3}>
-          <Image
-            src={
-              avatarImg.file.childImageSharp.gatsbyImageData.images.fallback.src
-            }
-            roundedCircle
-            alt="Prof. Dr. Christian Drumm"
-          />
-        </Col>
-        <Col>
-          <h1>Prof. Dr. Christian Drumm</h1>
-          <ul className="list-unstyled">
-            <li>
-              Professor of Information Systems @{" "}
-              <a
-                href="https://www.fh-aachen.de/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                FH Aachen
-              </a>
-            </li>
-            <li>
-              Founder, Coach and Consultant @{" "}
-              <a
-                href="https://www.trans4m.io/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                trans4mio
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://community.sap.com/programs/influencer-programs/champions"
-                target="_blank"
-                rel="noreferrer"
-              >
-                SAP Champion
-              </a>{" "}
-              and Member of the{" "}
-              <a
-                href="https://events.sap.com/de/sap-next-gen/en/contact"
-                target="_blank"
-                rel="noreferrer"
-              >
-                SAP DACH Academic Board
-              </a>
-            </li>
-            <li>
-              Co-Founder of the{" "}
-              <a
-                href="https://wiki.scn.sap.com/wiki/display/events/ABAP+CodeRetreat"
-                target="_blank"
-                rel="noreferrer"
-              >
-                ABAP CodeRetreat
-              </a>
-            </li>
-            <li>
-              Author @{" "}
-              <a
-                href="https://www.rheinwerk-verlag.de/einstieg-in-sap-s4hana/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Rheinwerk
-              </a>
-            </li>
-          </ul>
-        </Col>
-      </Row>
-
-      <Row>
-        <Col md={3}>
+          <Row>
+            <Image
+              src={
+                avatarImg.file.childImageSharp.gatsbyImageData.images.fallback
+                  .src
+              }
+              roundedCircle
+              alt="Prof. Dr. Christian Drumm"
+            />
+          </Row>
           <Row>
             <Col>
               <h2>
@@ -132,21 +68,103 @@ const HomePage = () => {
                 </a>
               </h2>
             </Col>
+            <Col>
+              <h2>
+                <a
+                  href="https://github.com/ceedee666"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <SiGithub />
+                </a>
+              </h2>
+            </Col>
           </Row>
         </Col>
+        <Col>
+          <Row>
+            <h1>Prof. Dr. Christian Drumm</h1>
+          </Row>
+          <Row>
+            <ul className="list-unstyled">
+              <li>
+                Professor of Information Systems @{" "}
+                <a
+                  href="https://www.fh-aachen.de/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  FH Aachen
+                </a>
+              </li>
+              <li>
+                Founder, Coach and Consultant @{" "}
+                <a
+                  href="https://www.trans4m.io/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  trans4mio
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://community.sap.com/programs/influencer-programs/champions"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  SAP Champion
+                </a>{" "}
+                and Member of the{" "}
+                <a
+                  href="https://events.sap.com/de/sap-next-gen/en/contact"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  SAP DACH Academic Board
+                </a>
+              </li>
+              <li>
+                Co-Founder of the{" "}
+                <a
+                  href="https://wiki.scn.sap.com/wiki/display/events/ABAP+CodeRetreat"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  ABAP CodeRetreat
+                </a>
+              </li>
+              <li>
+                Author @{" "}
+                <a
+                  href="https://www.rheinwerk-verlag.de/einstieg-in-sap-s4hana/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Rheinwerk
+                </a>
+              </li>
+              <li>
+                List of my{" "}
+                <a
+                  href="https://www.fh-aachen.de/menschen/drumm/veroeffentlichungen"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  publications
+                </a>
+              </li>
+            </ul>
+          </Row>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={3}>
+          <Row></Row>
+        </Col>
         <Col md={9}>
-          <ul className="list-unstyled">
-            <li>
-              List of my{" "}
-              <a
-                href="https://www.fh-aachen.de/menschen/drumm/veroeffentlichungen"
-                target="_blank"
-                rel="noreferrer"
-              >
-                publications
-              </a>
-            </li>
-          </ul>
+          <ul className="list-unstyled"></ul>
         </Col>
       </Row>
     </Layout>
